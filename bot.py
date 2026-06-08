99db9b8a4be48f1b9f8a6544ef5eb49051fc0266
natex8999-crypto <natex8999@gmail.com>
2026-06-08 14:35:04 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,9 @@
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 
-TOKEN = "BURAYA_TOKEN_YAZMA"
+import os
+
+TOKEN = os.getenv("BOT_TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
